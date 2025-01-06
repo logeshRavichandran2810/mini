36dd58cf17833696e4e3739adc56d85e97835476
logeshRavichandran2810 <logeshravichandran2810@gmail.com>
2025-01-06 23:02:37 +0530

    update

--- miniproject/blogproject/blogapp/views.py
+++ miniproject/blogproject/blogapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Blogger,Post
+from blogapp.forms import ViewPost
+from django.http import HttpResponse
+import os
 
 
 # Create your views here.
@@ -11,6 +14,17 @@ def home(request):
 
 
 def post(request):
-    return render(request,"blogapp/register.html")
+    posts=Post.objects.all()
+    return render(request,"blogapp/register.html",{"post":posts})
 
+def ViewPostList(request):
+        if request.method == 'POST':
+           form = ViewPost(request.POST)
+           if form.is_valid():
+               print(form.cleaned_data)
+               return HttpResponse('thank you')
+        else:
+           form = ViewPost()
 
+    # posts=ViewPost()
+        return render(request,"blogapp/form.html",{"form":form})
