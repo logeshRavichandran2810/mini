c681b07cffd68f142906ee0d0801172d87dcb86a
LOGESH <logeshravichandran2810@gmail.com>
2025-01-07 18:15:19 +0530

    Delete miniproject/blogproject directory

--- miniproject/blogproject/blogapp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Post
-class ViewPost(forms.ModelForm):
-    class Meta:
-        model=Post
-        fields=['title','date','image','content','description','blogger']
